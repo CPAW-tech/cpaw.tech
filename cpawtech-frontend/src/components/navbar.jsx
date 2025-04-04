334bfbd8a539de2ef3b31e32799a4d6ff40f5b80
regexbuster <benjamin@gofranks.com>
2025-04-04 14:30:50 -0400

    switch

--- cpawtech-frontend/src/components/navbar.jsx
+++ cpawtech-frontend/src/components/navbar.jsx
@@ -3,12 +3,43 @@ import './navbar.css'
 
 export function NavBar() {
     return (
-        <div className={'navbar'}>
-            <Link to={{ pathname: '/about' }}>About</Link>
-            <Link to={{ pathname: '/faq' }}>FAQ</Link>
-            {/* LOGO? */}
-            <Link to={{ pathname: '/contact' }}>Contact</Link>
-            <Link to={{ pathname: '/signup' }}>Sign Up</Link>
+        <div className={'navbar card'}>
+            <div className="linker">
+                <h1 className={'navelem'}>
+                    <strong>CPAW.TECH</strong>
+                </h1>
+                <nav className={'linksbar'}>
+                    <Link
+                        to={{ pathname: '/about' }}
+                        prefetch="intent"
+                        className={'navelem card'}
+                    >
+                        About
+                    </Link>
+                    <Link
+                        to={{ pathname: '/faq' }}
+                        prefetch="intent"
+                        className={'navelem card'}
+                    >
+                        FAQ
+                    </Link>
+
+                    <Link
+                        to={{ pathname: '/contact' }}
+                        prefetch="intent"
+                        className={'navelem card'}
+                    >
+                        Contact
+                    </Link>
+                    <Link
+                        to={{ pathname: '/signup' }}
+                        prefetch="intent"
+                        className={'navelem card'}
+                    >
+                        Sign Up
+                    </Link>
+                </nav>
+            </div>
         </div>
     )
 }
